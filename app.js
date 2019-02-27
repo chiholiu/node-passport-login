4bf9e97a54e18a96fa2c49e99dc1dfb02927e429
Chi Ho <>
2019-02-27 20:17:45 +0100

    flash and register form succeed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const expresslayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -20,10 +22,27 @@ app.set('view engine', 'ejs');
 // Bodyparser
 app.use(express.urlencoded({ extended: true }));
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUnitialized: true
+}));
+
+//Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+})
+
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
